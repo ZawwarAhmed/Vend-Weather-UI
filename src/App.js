13eb57226c8058cb7478460203ea5548aaf10b1c
ZawwarAhmed <zawwar.ahmed@venturedive.com>
2022-07-25 17:03:48 +0500

    updated App.js (added components)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import './App.css';
+import "./App.css";
+import CurrentWeather from "./components/CurrentWeather";
+import DailyWeather from "./components/DailyWeather";
+import Dropdown from "./components/Dropdown";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <CurrentWeather />
+      <DailyWeather />
     </div>
   );
 }
