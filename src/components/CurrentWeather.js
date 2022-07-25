7a8143ced1b6a561eef68196533d839e4c26fde5
ZawwarAhmed <zawwar.ahmed@venturedive.com>
2022-07-25 17:04:07 +0500

    added CurrentWeather component

---
+++ src/components/CurrentWeather.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "../stylesheets/CurrentWeather.css"
+
+function CurrentWeather() {
+  return (
+    <div className='current-weather'>
+        <div className="location">
+            <h2 className="city">
+                Karachi
+            </h2>
+            <h2 className="day">
+                Friday
+            </h2>
+            <h2 className="weather">Rainy</h2>
+        </div>
+
+        <h1 className="temperature">31°C</h1>
+    </div>
+  )
+}
+
+export default CurrentWeather
