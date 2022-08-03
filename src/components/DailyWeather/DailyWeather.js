a726fa8d3218187a760a6bbc2be2e3409f2cf71c
ZawwarAhmed <zawwar.ahmed@venturedive.com>
2022-08-03 19:50:21 +0500

    DailyWeather component updated

--- src/components/DailyWeather/DailyWeather.js
+++ src/components/DailyWeather/DailyWeather.js
@@ -1,15 +1,14 @@
 import React from "react";
-import { days, weather, temperature } from "../../data/weatherData";
 import "./DailyWeather.css"
 
-function DailyWeather() {
+function DailyWeather({ days, dailyWeather, dailyTemperature }) {
   return (
     <div className="daily-weather">
       {days.map((day, i) => (
         <div className="day-weather">
           <h2>{day}</h2>
-          <h4>{weather[i]}</h4>
-          <h3>{temperature[i]}°C</h3>
+          <h4>{dailyWeather[i]}</h4>
+          <h3>{dailyTemperature[i]}°C</h3>
         </div>
       ))}
     </div>
