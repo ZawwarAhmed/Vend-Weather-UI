fae85c884caf6e7afa23b29f1806457df1341808
ZawwarAhmed <zawwar.ahmed@venturedive.com>
2022-09-01 15:47:55 +0500

    update Navbar.js

--- src/components/common/Navbar/Navbar.js
+++ src/components/common/Navbar/Navbar.js
@@ -2,11 +2,11 @@ import React from "react";
 import Dropdown from "../Dropdown/Dropdown";
 import "./Navbar.css";
 
-function Navbar({ title, options, filterCityOptions }) {
+function Navbar({ title, options, filterCityOptions, setOption }) {
   return (
     <div className="Navbar">
       <h1 className="title">{title}</h1>
-      <Dropdown options={options} filterCityOptions={filterCityOptions} />
+      <Dropdown options={options} filterCityOptions={filterCityOptions} setOption={setOption} />
     </div>
   );
 }
