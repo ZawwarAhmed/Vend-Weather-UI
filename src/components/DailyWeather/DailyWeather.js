5eddac72ac0e129b9ee057b7f11099eff981ef20
ZawwarAhmed <zawwar.ahmed@venturedive.com>
2022-09-01 15:48:00 +0500

    update DailyWeather.js

--- src/components/DailyWeather/DailyWeather.js
+++ src/components/DailyWeather/DailyWeather.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./DailyWeather.css"
 
-function DailyWeather({ days, dailyWeather, dailyTemperature }) {
+function DailyWeather({ days, dailyWeather, dailyTemperature, weatherIcon, time }) {
   return (
     <div className="daily-weather">
       {days.map((day, i) => (
         <div key={i} className="day-weather">
           <h2>{day}</h2>
+          {/* <h2>{time}</h2> */}
+          <img className="weather-icon" src={`http://openweathermap.org/img/w/${weatherIcon[i]}.png`} alt="" />
           <h4>{dailyWeather[i]}</h4>
-          <h3>{dailyTemperature[i]}°C</h3>
+          <h3>{dailyTemperature[i]} °C</h3>
         </div>
       ))}
     </div>
