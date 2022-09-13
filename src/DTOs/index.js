3dd98d33191b549fce40eec74e48f47a7044d10d
ZawwarAhmed <zawwar.ahmed@venturedive.com>
2022-09-13 21:54:56 +0500

    feat: create src/DTOs/index.js

---
+++ src/DTOs/index.js
@@ -0,0 +1,9 @@
+import { getCitiesResponse } from "./getCitiesResponse";
+import { getCurrentWeatherResponse } from "./getCurrentWeatherResponse";
+import { getDailyWeatherResponse } from "./getDailyWeatherResponse";
+
+export {
+  getCitiesResponse,
+  getCurrentWeatherResponse,
+  getDailyWeatherResponse,
+};
