c21d61308bef1e5ff4f1bd5b50b7740b715f8d03
ZawwarAhmed <zawwar.ahmed@venturedive.com>
2022-09-13 21:54:33 +0500

    update CurrentWeather.js

--- src/components/CurrentWeather/CurrentWeather.js
+++ src/components/CurrentWeather/CurrentWeather.js
@@ -1,17 +1,18 @@
 import React from "react";
 import "./CurrentWeather.css";
 
-function CurrentWeather({
-  city,
-  day,
-  date,
-  time,
-  currentWeather,
-  currentTemperature,
-  feelsLike,
-  humidity,
-  weatherIcon
-}) {
+function CurrentWeather({currentWeatherData}) {
+  const {
+    city,
+    day,
+    date,
+    time,
+    weather,
+    temperature,
+    feelsLike,
+    humidity,
+    weatherIcon
+  } = currentWeatherData;
   const twelveHourFormat = new Date(`${date} ${time}`).toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3")
   return (
     <div className="current-weather">
@@ -20,11 +21,11 @@ function CurrentWeather({
         <h2 className="day">
           {day}, {twelveHourFormat}
         </h2>
-        <h2 className="weather">{currentWeather}</h2>
+        <h2 className="weather">{weather}</h2>
         <img src={`http://openweathermap.org/img/w/${weatherIcon}.png`} alt="" />
       </div>
       <div className="weather-figures">
-        <h1 className="temperature">{currentTemperature} °C</h1>
+        <h1 className="temperature">{temperature} °C</h1>
         <h3 className="feels-like">Feels Like {feelsLike} °C</h3>
         <h3 className="humidity">Humidity {humidity}</h3>
       </div>
