7c634837e49da667150411cf6764cd7ceab390a6
ZawwarAhmed <zawwar.ahmed@venturedive.com>
2022-09-13 21:54:30 +0500

    update App.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import DailyWeather from "./components/DailyWeather/DailyWeather";
 import Navbar from "./components/common/Navbar/Navbar";
 import { title } from "./data/appData";
 import { currentWeatherResponse } from "./data/currentWeatherData";
-import { days, dailyWeather, dailyTemperature, dailyWeatherResponse } from "./data/dailyWeatherData";
+import { dailyWeatherResponse } from "./data/dailyWeatherData";
 import { citiesResponse } from "./data/citiesData";
+import { getCitiesResponse, getDailyWeatherResponse, getCurrentWeatherResponse } from "./DTOs/index";
 import "./App.css";
 
 function App() {
-  const [citiesData, setCitiesData] = useState([]);
-  const [currentWeatherData, setCurrentWeatherData] = useState({});
-  const [dailyWeatherData, setDailyWeatherData] = useState([]);
+  const [cities, setCities] = useState([]);
+  const [currentWeather, setCurrentWeather] = useState({});
+  const [dailyWeather, setDailyWeather] = useState([]);
   const [options, setOptions] = useState([]);
   const [option, setOption] = useState(
     localStorage.getItem("city") || "Karachi"
@@ -19,22 +20,29 @@ function App() {
 
   useLayoutEffect(() => {
     async function fetchData() {
-      setCitiesData(await citiesResponse());
-      setCurrentWeatherData(await currentWeatherResponse(option));
-      setDailyWeatherData(await dailyWeatherResponse(option))
+      setCities(await citiesResponse());
+      setCurrentWeather(await currentWeatherResponse(option));
+      setDailyWeather(await dailyWeatherResponse(option));
     }
     fetchData();
   }, [option]);
 
+  const citiesData = new getCitiesResponse(cities);
+  const dailyWeatherData = dailyWeather.map(
+    (data) => new getDailyWeatherResponse(data)
+  );
+  const currentWeatherData = new getCurrentWeatherResponse(currentWeather);
+  currentWeather.city = option;
+
   const filterCityOptions = (text) => {
     if (text.length >= 4) {
       setOptions(
-        citiesData.filter((option) =>
+        citiesData.cities.filter((option) =>
           option.toLowerCase().includes(text.toLowerCase())
         )
       );
     } else {
-      setOptions(citiesData.filter((city) => city.length < 4));
+      setOptions(citiesData.cities.filter((city) => city.length < 4));
     }
   };
 
@@ -46,24 +54,8 @@ function App() {
         filterCityOptions={filterCityOptions}
         setOption={setOption}
       />
-      <CurrentWeather
-        city={option}
-        day={currentWeatherData.day}
-        date={currentWeatherData.date}
-        time={currentWeatherData.time}
-        currentWeather={currentWeatherData.weather}
-        currentTemperature={currentWeatherData.temperature}
-        feelsLike={currentWeatherData.feelsLike}
-        humidity={currentWeatherData.humidity}
-        weatherIcon={currentWeatherData.weatherIcon}
-      />
-      <DailyWeather
-        days={dailyWeatherData.map(item => item.day)}
-        dailyWeather={dailyWeatherData.map(item => item.weather)}
-        weatherIcon={dailyWeatherData.map(item => item.weatherIcon)}
-        dailyTemperature={dailyWeatherData.map(item => item.temperature)}
-        time={dailyWeatherData.map(item => item.time)}
-      />
+      <CurrentWeather currentWeatherData={currentWeatherData} />
+      <DailyWeather dailyWeatherData={dailyWeatherData} />
     </div>
   );
 }
